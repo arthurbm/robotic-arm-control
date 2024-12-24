c845f13ae93e65b018b59b4261edd99f3cc5351f
Mathbonc <mjbo@cin.ufpe.br>
2024-12-24 12:23:55 -0300

    feat(Q2): Adicionada função IK

--- project1/Q1.py
+++ project1/Q1.py
@@ -42,6 +42,26 @@ def fk(theta1, theta2):
     E = SE2_theta(theta1) @ SE2_xy(a,0) @ SE2_theta(theta2) @ SE2_xy(a, 0)
     return E[0][2], E[1][2], theta1+theta2
 
+def ik(x,y):
+    # tamanho dos dois links a = 1
+    a = 1
+
+    r2 = x**2 + y**2
+    cos_theta2 = (r2 - (a**2 + a**2)) / (2 * a * a)
+    
+    # Checa se o valor é possível
+    if cos_theta2 < -1.0 or cos_theta2 > 1.0:
+        raise ValueError("Posição inválida.")
+    
+    sin_theta2 = math.sqrt(1 - cos_theta2**2)  
+    theta2 = math.atan2(sin_theta2, cos_theta2)
+
+    # Cálculo de theta1
+    num = a * math.sin(theta2)
+    denom = a + a * math.cos(theta2)
+    theta1 = math.atan2(y, x) - math.atan2(num, denom)
+
+    return theta1, theta2
 
 # === TESTES ===
 
@@ -80,6 +100,6 @@ def runTests2():
     print(fk(math.pi/2,-math.pi/2))
     print(fk(-math.pi,math.pi))
 
-runTests2()
 runTests1()
+runTests2()
 

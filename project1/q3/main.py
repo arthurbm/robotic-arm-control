af2832924abfb23cd9cb595dee442093a0cb418c
Mathbonc <mjbo@cin.ufpe.br>
2024-12-25 21:37:43 -0300

    feat: Added initial implementation for Q3 in Project 1

---
+++ project1/q3/main.py
@@ -0,0 +1,12 @@
+import numpy as np
+import math
+from ..q1.main import SE2_theta, SE2_xy
+from ..q2.main import ik,fk
+
+def traj_joint(theta1_init, theta2_init, theta1_final, theta2_final):
+    TS = 0.01 
+    return
+
+def traj_eucl(x_init, y_init, x_final, y_final):
+    TS = 0.01 
+    return
